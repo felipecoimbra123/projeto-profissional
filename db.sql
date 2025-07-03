1a031db4521cbacd4f54ed9e56fd731cbeb997ed
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2025-07-03 16:54:51 -0300

    Arquivo do banco de dados adicionado ao código

---
+++ db.sql
@@ -0,0 +1,56 @@
+create database fotografia_pp;
+use fotografia_pp;
+
+create table usuario(
+	id int primary key auto_increment,
+    nome varchar(255) not null,
+    email varchar(255) not null unique,
+    senha varchar(255) not null,
+    imagemArtigo varchar(255),
+    seguidores int,
+    imagemPerfil varchar(255),
+    criadoEm timestamp default current_timestamp
+);
+
+create table seguir(
+	seguidor int,
+    seguindo int,
+    foreign key (seguidor) references usuario(id),
+    foreign key (seguindo) references usuario(id)
+);
+
+create table artigo(
+	id int primary key auto_increment,
+    titulo varchar(255),
+    conteudo varchar(255),
+    imagemArtigo varchar(255),
+    categoria varchar(255),
+    criadoEm timestamp default current_timestamp,
+    autor_id int,
+    foreign key (autor_id) references usuario(id)
+);
+
+create table fotografia(
+	id int primary key auto_increment,
+    titulo varchar(255) not null,
+    descricao varchar(255) not null,
+    url varchar(255) not null,
+    media_avaliacao float not null,
+    curtidas int,
+    autor_id int,
+    comentario int,
+    foreign key (autor_id) references usuario(id),
+    foreign key (comentario) references comentario(id)
+);
+
+create table comentario(
+	id int primary key auto_increment,
+    texto varchar(255) not null,
+    fotografia int,
+    autor_id int,
+    foreign key (fotografia) references fotografia(id),
+    foreign key (autor_id) references usuario(id),
+    criadoEm timestamp default current_timestamp
+);
+
+select * from usuario;
